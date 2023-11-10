4535b5affae3ff11bf82c372b70a136613aeaeef
jihwan2843 <jihwan2843@gmail.com>
2023-11-10 17:45:09 +0900

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import CoinCard from "./components/CoinCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [coinPrices, setCoinPrices] = useState();
+
+  const getCoinPrices = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-MATIC,BTC-ASTR"
+      );
+      console.log(response);
+      setCoinPrices(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getCoinPrices();
+  }, []);
+
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {coinPrices ? (
+        <ul className="w-96">
+          <Slider
+            autoplay={true}
+            autoplaySpeed={2000}
+            arrows={false}
+            dots={true}
+          >
+            {coinPrices.map((v, i) => {
+              return (
+                <CoinCard
+                  key={i}
+                  tokenPrice={v}
+                  btcPrice={coinPrices[0].trade_price}
+                />
+              );
+            })}
+          </Slider>
+        </ul>
+      ) : (
+        <div>로딩중......</div>
+      )}
+    </div>
+  );
 };
 
 export default App;
